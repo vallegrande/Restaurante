127ef3d055048b079603870881fd96c60d51671c
Usuario <Usuario@192.168.1.39>
2019-01-22 23:29:15 -0500

    creando paquetes

---
+++ src/modelo/Sucursal.java
@@ -0,0 +1,32 @@
+package modelo;
+
+public class Sucursal {
+    private int codSucur;
+    private String direcSucur;
+    private String telfSucur;
+
+    public int getCodSucur() {
+        return codSucur;
+    }
+
+    public void setCodSucur(int codSucur) {
+        this.codSucur = codSucur;
+    }
+
+    public String getDirecSucur() {
+        return direcSucur;
+    }
+
+    public void setDirecSucur(String direcSucur) {
+        this.direcSucur = direcSucur;
+    }
+
+    public String getTelfSucur() {
+        return telfSucur;
+    }
+
+    public void setTelfSucur(String telfSucur) {
+        this.telfSucur = telfSucur;
+    }
+    
+}
