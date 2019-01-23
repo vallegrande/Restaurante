7eac87bc97c1d0cffe1ab7f22423370b449d2369
Usuario <Usuario@192.168.1.39>
2019-01-22 23:54:09 -0500

    mesa, ubigeo

---
+++ src/modelo/Mesa.java
@@ -0,0 +1,33 @@
+package modelo;
+
+public class Mesa {
+    private int codMesa;
+    private String numMesa;
+    private String capMesa;
+
+    public int getCodMesa() {
+        return codMesa;
+    }
+
+    public void setCodMesa(int codMesa) {
+        this.codMesa = codMesa;
+    }
+
+    public String getNumMesa() {
+        return numMesa;
+    }
+
+    public void setNumMesa(String numMesa) {
+        this.numMesa = numMesa;
+    }
+
+    public String getCapMesa() {
+        return capMesa;
+    }
+
+    public void setCapMesa(String capMesa) {
+        this.capMesa = capMesa;
+    }
+    
+    
+}
--- src/modelo/Ubigeo.java
+++ src/modelo/Ubigeo.java
@@ -1,5 +1,42 @@
 package modelo;
 
 public class Ubigeo {
+    private String numUbi;
+    private String depUbi;
+    private String proUbi;
+    private String distUbi;
+
+    public String getNumUbi() {
+        return numUbi;
+    }
+
+    public void setNumUbi(String numUbi) {
+        this.numUbi = numUbi;
+    }
+
+    public String getDepUbi() {
+        return depUbi;
+    }
+
+    public void setDepUbi(String depUbi) {
+        this.depUbi = depUbi;
+    }
+
+    public String getProUbi() {
+        return proUbi;
+    }
+
+    public void setProUbi(String proUbi) {
+        this.proUbi = proUbi;
+    }
+
+    public String getDistUbi() {
+        return distUbi;
+    }
+
+    public void setDistUbi(String distUbi) {
+        this.distUbi = distUbi;
+    }
     
+        
 }
